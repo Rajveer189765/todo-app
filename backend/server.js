d8aa19b7f58aaead1a9dd9090a61d7c749618243
Rajveer189765 <rajveer10rajpuria@gmail.com>
2026-06-30 10:42:44 +0530

    connected mongoDB locally

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.get("/", (req, res) => {
     res.send("Welcome to Todo App API");
